c40bec29754db1a961f78e74f2d966248f92a35b
mrunal08 <73746021+mrunal08@users.noreply.github.com>
2021-07-05 18:59:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var database;
+var position;
+var canvas;
+var playerCount;
+var form,game,player;
+
+function setup(){
+  canvas=createCanvas(400,400)
+  database = firebase.database();
+  
+}
+
+function draw(){
+  background("white");
+  
+}
